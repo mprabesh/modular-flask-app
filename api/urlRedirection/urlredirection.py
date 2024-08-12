0abb5e575a85b94029430b45b2d227cf9f216a8c
mprabesh <theprabesh723@gmail.com>
2024-08-11 23:36:01 -0400

    feat: redirection code added

--- api/urlRedirection/urlredirection.py
+++ api/urlRedirection/urlredirection.py
@@ -5,12 +5,12 @@ from init_db import get_db
 url_redirection_bp=Blueprint("url_redirection_bp",__name__)
 
 
-# @url_redirection_bp.route("/<short_link_param>",methods=["GET"])
-# def redirect_url(short_link_param):
-#     try:
-#         if short_link_param:
-#             print(short_link_param)
-#             original_url=get_user_URL(short_link_param)
-#             return redirect(original_url, 302)
-#     except Exception as e:
-#         print(e)
+@url_redirection_bp.route("/<short_link_param>",methods=["GET"])
+def redirect_url(short_link_param):
+    try:
+        if short_link_param:
+            print(short_link_param)
+            original_url=get_user_URL(short_link_param)
+            return redirect(original_url, 302)
+    except Exception as e:
+        print(e)
